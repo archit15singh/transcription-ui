1cad72c4f55150fffdc124bf78a61af26faa540f
Archit Singh <contact.architsingh@gmail.com>
2023-11-14 18:22:20 +0530

    test 1.wav

--- app.py
+++ app.py
@@ -4,21 +4,26 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/get_transcription')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/get_transcription")
 def get_transcription():
-    json_file_path = Path(__file__).resolve().parent / 'static' / 'transcriptions' / 'whisper_response.json'
+    json_file_path = (
+        Path(__file__).resolve().parent / "static" / "transcriptions" / "1.json"
+    )
     try:
-        with open(json_file_path, 'r') as json_file:
+        with open(json_file_path, "r") as json_file:
             transcription_data = json.load(json_file)
-        return jsonify(transcription_data['transcription'])
+        return jsonify(transcription_data["transcription"])
     except FileNotFoundError:
         return jsonify({"error": "JSON file not found"}), 404
     except json.JSONDecodeError:
         return jsonify({"error": "Error decoding JSON"}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
