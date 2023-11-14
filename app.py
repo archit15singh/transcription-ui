ccb399743db6bc047813cc37a8870a80f0e31cb4
Archit Singh <131599737+archit15singh@users.noreply.github.com>
2023-11-14 23:28:04 +0000

    add query params based audio render

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import json
 from pathlib import Path
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    audio_id = request.args.get('id', default='1', type=str)
+    return render_template("index.html", audio_id=audio_id)
 
 
 @app.route("/get_transcription")
 def get_transcription():
+    audio_id = request.args.get('id', default='1', type=str)
     json_file_path = (
-        Path(__file__).resolve().parent / "static" / "transcriptions" / "1.json"
+        Path(__file__).resolve().parent / "static" / "transcriptions" / f"{audio_id}.json"
     )
     try:
         with open(json_file_path, "r") as json_file:
